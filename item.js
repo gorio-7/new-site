b042393a480a6b9f65758d9b8a6e2e5b30fadba2
gorio-7 <black.kz717@gmail.com>
2026-01-08 23:19:25 +0900

    Add files via upload

--- item.js
+++ item.js
@@ -3,6 +3,23 @@ function ChangeImage(subImage) {
   main.src = subImage.src;
 }
 
+window.addEventListener("load", () => {
+  document.querySelector(".subImage img").classList.add("active");
+});
+
+function ChangeImage(subImage) {
+  const main = document.getElementById("mainImage");
+  main.src = subImage.src;
+
+  document.querySelectorAll(".subImage img").forEach(img => {
+    img.classList.remove("active");
+  });
+
+  subImage.classList.add("active");
+}
+
+
+
 const minusBtn = document.querySelector(".minus");
 const plusBtn = document.querySelector(".plus");
 const countInput = document.querySelector(".count");
